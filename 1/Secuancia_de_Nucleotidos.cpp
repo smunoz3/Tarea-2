43e287239a9a5043e937a9ff008bead31a80631c
Neichoo <gomez.ignacio09@gmail.com>
2023-05-26 01:42:47 -0400

    header y algo del main

--- 1/Secuancia_de_Nucleotidos.cpp
+++ 1/Secuancia_de_Nucleotidos.cpp
@@ -3,7 +3,30 @@
 #include "Secuancia_de_Nucleotidos.hpp"
 using namespace std;
 
-int main(){
-    
+#include <iostream>
+
+int main() {
+    int i;
+    char n;
+    tListaNucleotidos lista;
+    ifstream fp;
+    fp.open("secuencias_nucleotidos.txt");
+    if (!fp.is_open()){
+        cout << "Error al abrir el archivo" << endl;
+        return -1;
+    }
+    int longSec, cantSec;
+    string Sec, operacion;
+    if (operacion == "INSERTAR"){
+        lista.Inserciones(i, n);
+    }
+    else if (operacion == "BORRAR"){
+        lista.Borrados(i);
+    }
+    else if (operacion == "INTERCAMBIAR"){
+        lista.Intercambios(i, n);
+    }
+
+
     return 0;
 }
--- 1/Secuancia_de_Nucleotidos.hpp
+++ 1/Secuancia_de_Nucleotidos.hpp
@@ -17,19 +17,85 @@ private:
 public:
     tListaNucleotidos();
     ~tListaNucleotidos(){};
+    void moveToStart();
+    void moveToEnd();
+    void prev();
+    void next();
+    void moveToPos(int i);
     void Inserciones(int i, char n);
     void Borrados(int i);
     void Intercambios(int i, char n);
 };
 
 tListaNucleotidos::tListaNucleotidos() {
+    //Constructor de la lista de nucleotidos
     head = tail = curr = new tNodo;
     listSize = 0;
     pos = 0;
 }
 
 tListaNucleotidos::~tListaNucleotidos() {
-    // Aquí puedes realizar las tareas de limpieza necesarias para liberar recursos
+    //Este es el destructor de la lista de nucleotidos
+    curr = head;
+    while (curr != nullptr) {
+        tNodo* next = curr->sig;
+        delete curr;
+        curr = next;
+}
+}
+
+void tListaNucleotidos::moveToStart(){
+    //Moverse al inicio de la lista
+    curr = head;
+    pos = 0;
+    return ;
+}
+
+void tListaNucleotidos::moveToEnd(){
+    //Moverse al final de la lista
+    curr = tail;
+    pos = listSize;
+    return;
+}
+
+void tListaNucleotidos::prev(){
+    //Moverse a la posición previa
+    tNodo* temp;
+    if (curr == head){
+        return;
+    }
+    temp = head;
+    while (temp->sig != curr){
+        temp = temp->sig;
+    }
+    curr = temp;
+    pos--;
+    return;
+}
+
+void tListaNucleotidos::next(){
+    //Moverse a la siguiente posición
+    if (curr != tail){
+        curr = curr->sig;
+        pos++;
+    }
+    return;
+}
+
+void tListaNucleotidos::Inserciones(int i, char n) {
+    if (i < 0 || i > listSize) {
+        return;
+    }
+    moveToPos(i);
+    tNodo* aux = new tNodo;
+    aux->nucleotido = n;
+    aux->sig = curr->sig;
+    curr->sig = aux;
+    if (curr->sig == nullptr) {
+        tail = aux;
+    }
+    listSize++;
+    return;
 }
 
 void tListaNucleotidos::Inserciones(int i, char n) {
@@ -37,10 +103,7 @@ void tListaNucleotidos::Inserciones(int i, char n) {
     if (i < 0 || i > listSize){
         return ;
     }
-    curr = head;
-    for(int j=0; j < i; j++){
-        curr = curr->sig;
-    }
+    
     tNodo* aux = curr->sig;
     curr->sig = new tNodo;
     curr->sig->nucleotido = n;
@@ -53,11 +116,34 @@ void tListaNucleotidos::Inserciones(int i, char n) {
 }
 
 void tListaNucleotidos::Borrados(int i) {
-    // Implementación del método Borrados
+    //Borrar el nucleotido en la posición i
+    if (i < 0 || i >= listSize) {
+        return;
+    }
+    moveToPos(i - 1);
+    tNodo* nodoAnterior = curr;
+    if (nodoAnterior->sig == nullptr) {
+        return;
+    }
+    tNodo* nodoBorrado = nodoAnterior->sig;
+    nodoAnterior->sig = nodoBorrado->sig;
+    if (nodoBorrado == tail) {
+        tail = nodoAnterior;
+    }
+    delete nodoBorrado;
+    listSize--;
     return;
 }
 
 void tListaNucleotidos::Intercambios(int i, char n) {
-    // Implementación del método Intercambios
+    //Intercambiar el nucleotido de la posición i por el nucleotido n
+    if (i < 0 || i >= listSize) {
+        return;
+    }
+    moveToPos(i);
+    if (curr == nullptr) {
+        return;
+    }
+    curr->nucleotido = n;
     return;
 }
